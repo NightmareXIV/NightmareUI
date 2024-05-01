a199c3bcd094753de28bd64b567c9cd7acd0acb1
Limiana <5073202+Limiana@users.noreply.github.com>
2024-05-01 05:42:53 +0300

    let's try this

--- NightmareUI/PrimaryUI/NuiBuilder.cs
+++ NightmareUI/PrimaryUI/NuiBuilder.cs
@@ -1,4 +1,5 @@
 ﻿using ECommons.ImGuiMethods;
+using FFXIVClientStructs.FFXIV.Client.UI;
 using ImGuiNET;
 using NightmareUI.PrimaryUI.Components;
 using System;
@@ -8,6 +9,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace NightmareUI.PrimaryUI;
 public class NuiBuilder
@@ -34,6 +36,37 @@ public class NuiBuilder
 		{
 				EnsureSectionNotNull();
 				CurrentSection.Widgets.Add(new ImGuiWidget(name, drawAction, help));
+				return this;
+		}
+
+		public delegate ref bool RefBoolDelegate();
+		public NuiBuilder Checkbox(string name, RefBoolDelegate value, string? help = null)
+		{
+				EnsureSectionNotNull();
+				CurrentSection.Widgets.Add(new ImGuiWidget(name, (x) => ImGui.Checkbox(x, ref value()), help));
+				return this;
+		}
+
+		public delegate ref int RefIntDelegate();
+		public NuiBuilder InputInt(float width, string name, RefIntDelegate value, string? help = null)
+		{
+				EnsureSectionNotNull();
+				CurrentSection.Widgets.Add(new ImGuiWidget(name, (x) =>
+				{
+						ImGui.SetNextItemWidth(width);
+						ImGui.InputInt(name, ref value());
+				}, help));
+				return this;
+		}
+
+		public NuiBuilder SliderInt(float width, string name, RefIntDelegate value, int min, int max, string? help = null, ImGuiSliderFlags flags = ImGuiSliderFlags.None)
+		{
+				EnsureSectionNotNull();
+				CurrentSection.Widgets.Add(new ImGuiWidget(name, (x) =>
+				{
+						ImGui.SetNextItemWidth(width);
+						ImGuiEx.SliderInt(name, ref value(), min, max, "%d", flags);
+				}, help));
 				return this;
 		}
 
