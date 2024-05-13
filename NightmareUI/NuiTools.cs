561d2e12aec4a931dfa55e36647cabc1118ee0c7
Limiana <5073202+Limiana@users.noreply.github.com>
2024-05-13 05:25:00 +0300

    Update tools

--- NightmareUI/NuiTools.cs
+++ NightmareUI/NuiTools.cs
@@ -1,6 +1,8 @@
 ﻿using Dalamud.Interface.Utility;
+using ECommons;
 using ECommons.ExcelServices.TerritoryEnumeration;
 using ECommons.ImGuiMethods;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using ImGuiNET;
 using System;
 using System.Collections.Generic;
@@ -12,10 +14,65 @@ using System.Threading.Tasks;
 namespace NightmareUI;
 public static class NuiTools
 {
-		public static void InitializeSingletonWindows()
+		private static Dictionary<string, int> ActiveTab = [];
+		public static void ButtonTabs(ButtonInfo[] buttons, int maxButtons = int.MaxValue) => ButtonTabs(GenericHelpers.GetCallStackID(), buttons, maxButtons);
+		public static void ButtonTabs(string id, ButtonInfo[] buttons, int maxButtons = int.MaxValue)
 		{
-
+				buttons = buttons.Where(x => x != null).ToArray();
+				maxButtons = Math.Clamp(maxButtons, 1, buttons.Length);
+				if (!ActiveTab.ContainsKey(id)) ActiveTab[id] = 0;
+				var width = ImGui.GetContentRegionAvail().X / maxButtons;
+				for (int i = 0; i < buttons.Length; i++)
+				{
+						var b = buttons[i];
+						var act = ActiveTab[id] == i;
+						ImGui.PushStyleVar(ImGuiStyleVar.FrameRounding, 0f);
+						if (act)
+						{
+								ImGui.PushStyleColor(ImGuiCol.Button, ImGui.GetStyle().Colors[(int)ImGuiCol.ButtonActive]);
+								ImGui.PushStyleColor(ImGuiCol.ButtonHovered, ImGui.GetStyle().Colors[(int)ImGuiCol.ButtonActive]);
+						}
+						var w = width;
+						if((i+1) % maxButtons == 0)
+						{
+								w = ImGui.GetContentRegionAvail().X;
+						}
+						if(ImGui.Button(b.Name, new(w, ImGui.GetFrameHeight())))
+						{
+								ActiveTab[id] = i;
+						}
+						if((i+1) % maxButtons != 0 && i+1 != buttons.Length)
+						{
+								ImGui.SameLine(0,0);
+						}
+						if (act)
+						{
+								ImGui.PopStyleColor(2);
+						}
+						ImGui.PopStyleVar();
+				}
+				try
+				{
+						buttons[ActiveTab[id]].Action();
+				}
+				catch (Exception e)
+				{
+						e.Log();
+				}
 		}
+
+		public record class ButtonInfo
+		{
+				public readonly string Name;
+				public readonly Action Action;
+
+				public ButtonInfo(string name, Action action)
+				{
+						Name = name ?? throw new ArgumentNullException(nameof(name));
+						Action = action ?? throw new ArgumentNullException(nameof(action));
+				}
+		}
+
 
 		public static bool RenderResidentialIcon(this uint residentialAetheryte, float? size = null)
 		{
