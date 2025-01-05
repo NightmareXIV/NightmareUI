96732a5020639dfa4421ef0874e66b2afda138d5
Limiana <5073202+Limiana@users.noreply.github.com>
2025-01-05 23:12:37 +0300

    Add data contract attributes

--- NightmareUI/Censoring/CensorConfig.cs
+++ NightmareUI/Censoring/CensorConfig.cs
@@ -1,13 +1,15 @@
 ﻿using ECommons.Configuration;
 using System;
 using System.Reflection;
+using System.Runtime.Serialization;
 
 namespace NightmareUI.Censoring;
 
 [Serializable]
+[DataContract(Name = "CensorConfig")]
 public class CensorConfig : IEzConfig
 {
-    [Obfuscation(Exclude = true)] public string Seed = Guid.NewGuid().ToString();
-    [Obfuscation(Exclude = true)] public bool Enabled = false;
-    [Obfuscation(Exclude = true)] public bool LesserCensor = false;
+    [DataMember(Name = "Seed")] [Obfuscation(Exclude = true)] public string Seed = Guid.NewGuid().ToString();
+    [DataMember(Name = "Enabled")][Obfuscation(Exclude = true)] public bool Enabled = false;
+    [DataMember(Name = "LesserCensor")][Obfuscation(Exclude = true)] public bool LesserCensor = false;
 }
